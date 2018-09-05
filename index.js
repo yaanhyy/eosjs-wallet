7954fa6d167052770c2ad0a084e89670f33c2d1c
洪远洋 <hongyuanyang@huobi.com>
2018-09-05 10:02:29 +0800

    add get_actions\Delegatebw func and  post func to pass signed json to  backend

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const Eos = require('eosjs');
+const EosApi = require('eosjs-api');
+const http = require('http');
+const querystring = require('querystring');
 
 let config = {
     expireInSeconds: 60,
@@ -13,9 +16,9 @@ let config = {
 };
 
 var eos = Eos(config);
+var eosapi = EosApi(config);
 
-
-async function asyncFunction(config) {
+async function asyncTransfer(config) {
 // Prepare headers
     expireInSeconds = 60 * 60 // 1 hour
     info = await eos.getInfo({})
@@ -59,7 +62,7 @@ async function asyncNewAccount(config) {
     pubkey = 'EOS68hmKN91AnKr5WaRWNgCFDbLCVmcBi48y9tDVUMMJD9W4zQFL8'
     eos = Eos({httpEndpoint: null, chainId:config.chainId,keyProvider:config.keyProvider, transactionHeaders})
     //transfer = await eos.newaccount('yyloveuu1314', 'hongyuanyang', 'EOS68hmKN91AnKr5WaRWNgCFDbLCVmcBi48y9tDVUMMJD9W4zQFL8','EOS8YrihssLWj51u1MRNoJURDawkeBuEq4sQrgDhFskoEZYCLqYyi',false)
-    transfer =await eos.transaction(tr => {
+    newaccpunt =await eos.transaction(tr => {
         tr.newaccount({
             creator: 'yyloveuu1314',
             name: 'hongyuanyang',
@@ -81,17 +84,92 @@ async function asyncNewAccount(config) {
             transfer: 0
         })
     })
-    transferTransaction = transfer.transaction
-    processedTransaction =await eos.pushTransaction(transferTransaction)
+    newaccpuntTransaction = newaccpunt.transaction
+    processedTransaction =await eos.pushTransaction(newaccpuntTransaction)
 }
 
+
+async function asyncgetActions(config) {
+// Prepare headers
+    expireInSeconds = 60 * 60 // 1 hour
+    info = await eos.getInfo({})
+    chainDate = new Date(info.head_block_time + 'Z')
+    expiration = new Date(chainDate.getTime() + expireInSeconds * 1000)
+    expiration = expiration.toISOString().split('.')[0]
+
+    block = await eos.getBlock(info.last_irreversible_block_num)
+
+    transactionHeaders = {
+        expiration,
+        ref_block_num: info.last_irreversible_block_num &0xffff,
+        ref_block_prefix: block.ref_block_prefix
+    }
+// OFFLINE (bring `transactionHeaders`)'https://api1.eosdublin.io'
+    eosapi = EosApi({httpEndpoint: config.httpEndpoint, chainId:config.chainId,keyProvider:config.keyProvider, transactionHeaders})
+    transfer = await eosapi.getActions('yyloveuu1314', 0, 10)
+    transferTransaction = transfer.transaction
+
+    //processedTransaction =await eos.pushTransaction(transferTransaction)
+
+}
+
+async function asyncDelegatebw(config) {
+// Prepare headers
+    expireInSeconds = 60 * 60 // 1 hour
+    info = await eos.getInfo({})
+    chainDate = new Date(info.head_block_time + 'Z')
+    expiration = new Date(chainDate.getTime() + expireInSeconds * 1000)
+    expiration = expiration.toISOString().split('.')[0]
+
+    block = await eos.getBlock(info.last_irreversible_block_num)
+
+    transactionHeaders = {
+        expiration,
+        ref_block_num: info.last_irreversible_block_num &0xffff,
+        ref_block_prefix: block.ref_block_prefix
+    }
+// OFFLINE (bring `transactionHeaders`)'https://api1.eosdublin.io'
+    eos = Eos({httpEndpoint: null, chainId:config.chainId,keyProvider:config.keyProvider, transactionHeaders})
+    transfer = await eos.delegatebw('yyloveuu1314','yyloveuu1314', '0.0000 EOS','0.5000 EOS', 0)
+    transferTransaction = transfer.transaction
+    var contents = JSON.stringify(transferTransaction);
+    var options = {
+        hostname: '127.0.0.1',
+        port: 9082,
+        path:'/eosmix/chain/transaction/push',
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'Content-Length':contents.length
+        }
+    }
+
+    var req = http.request(options, function(res){
+        res.setEncoding('utf8');
+        res.on('data',function(data){
+            console.log("data:",data);   //一段html代码
+        });
+    });
+
+    req.write(contents);
+    req.end;
+    //processedTransaction =await eos.pushTransaction(transferTransaction)
+}
+
+/*
 asyncNewAccount(config).then(data => { console.log(data); //asyncFunction return 的内容在这里获取
     }).catch(error => { console.log(error); // asyncFunction 的错误统一在这里抓取
     });
 
 
-asyncFunction(config).then(data => { console.log(data); //asyncFunction return 的内容在这里获取
+asyncTransfer(config).then(data => { console.log(data); //asyncFunction return 的内容在这里获取
  }).catch(error => { console.log(error); // asyncFunction 的错误统一在这里抓取
  });
 
-
+asyncgetActions(config).then(data => { console.log(data); //asyncFunction return 的内容在这里获取
+}).catch(error => { console.log(error); // asyncFunction 的错误统一在这里抓取
+});
+*/
+asyncDelegatebw(config).then(data => { console.log(data); //asyncFunction return 的内容在这里获取
+}).catch(error => { console.log(error); // asyncFunction 的错误统一在这里抓取
+});
